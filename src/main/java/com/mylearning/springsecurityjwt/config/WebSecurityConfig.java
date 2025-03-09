484403fc327200b2cd9c99dec6697cda61b271c2
prashant <prashanttiwary10@gmail.com>
2025-03-09 13:06:31 +0530

    InMemoryUserDetailsManager

--- src/main/java/com/mylearning/springsecurityjwt/config/WebSecurityConfig.java
+++ src/main/java/com/mylearning/springsecurityjwt/config/WebSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +21,36 @@ public class WebSecurityConfig {
         httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable());
+                .csrf(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        UserDetails user3 = User.builder()
+                .username("user3")
+                .password("{noop}password")
+                .roles("USER", "ADMIN")  // Will automatically set int the authorities ROLE_ADMIN and ROLE_USER
+                .build();
+
+        UserDetails user4 = User.builder()
+                .username("user4")
+                .password("{noop}password")
+                .authorities("ROLE_USER", "ROLE_ADMIN") // if we add roles like USER and ADMIN, then it will Set as ROLE_USER and ROLE_ADMIN in authorities
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
     }
 }
